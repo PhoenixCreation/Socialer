476b3732d62d8d04dd9e9c0d3f98839a2cf4395b
phoenix <phs28052001@gmail.com>
2020-09-22 12:24:25 +0530

    Updates

---
+++ precache-manifest.42f581217bbbd3cd65d36a84f9abefe5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9785f37aae423865ae1f30894a4c3f1a",
+    "url": "/Socialer/index.html"
+  },
+  {
+    "revision": "50f701469b6c08a3cf10",
+    "url": "/Socialer/static/css/main.4ba27310.chunk.css"
+  },
+  {
+    "revision": "0529a3d9a30f9895751e",
+    "url": "/Socialer/static/js/2.81014c87.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/Socialer/static/js/2.81014c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50f701469b6c08a3cf10",
+    "url": "/Socialer/static/js/main.a2ad9f34.chunk.js"
+  },
+  {
+    "revision": "3d32b0b4db0ab8619ca0",
+    "url": "/Socialer/static/js/runtime-main.b1eb37f6.js"
+  }
+]);
